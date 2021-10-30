08cd17c2ce099a475b99f67d70cf210c5441f617
vitorsoaresf <vitorsoaresf@gmail.com>
2021-10-30 08:03:43 -0300

    feat: implements style shopping cart

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -3,13 +3,25 @@ import "./styles.css";
 
 function ShoppingCart({ currentSale, removeProduct }) {
   return (
-    <ul className="list-cart">
-      {currentSale.map((product, index) => (
-        <li key={index}>
-          <ProductCart removeProduct={removeProduct} product={product} />
-        </li>
-      ))}
-    </ul>
+    <div className="shoppingCart">
+      <header className="shoppingCart__title">
+        <p>Carrinho de compras</p>
+      </header>
+      {currentSale.length ? (
+        <ul className="list-cart">
+          {currentSale.map((product, index) => (
+            <li key={index}>
+              <ProductCart removeProduct={removeProduct} product={product} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="list-clear">
+          <h3>Sua sacola está vazia!</h3>
+          <p>Adicione itens</p>
+        </div>
+      )}
+    </div>
   );
 }
 
